5605f809dbeb3b2f52cfadd5f145f039e14b9aea
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2024-05-13 15:28:26 +0200

    Link commit and created the general application theme

---
+++ app/src/main/java/com/tecknobit/neutron/activities/navigation/Splashscreen.kt
@@ -0,0 +1,68 @@
+package com.tecknobit.neutron.activities.navigation
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tecknobit.neutron.R
+import com.tecknobit.neutron.activities.session.MainActivity
+import com.tecknobit.neutron.ui.theme.AppTypography
+import com.tecknobit.neutron.ui.theme.NeutronTheme
+import com.tecknobit.neutron.ui.theme.displayFontFamily
+
+@SuppressLint("CustomSplashScreen")
+class Splashscreen : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NeutronTheme {
+                Box (
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text (
+                        text = getString(R.string.app_name),
+                        color = Color.White,
+                        style = AppTypography.displayLarge,
+                        fontSize = 55.sp,
+                    )
+                    Row (
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(30.dp),
+                        verticalAlignment = Alignment.Bottom,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "by Tecknobit",
+                            color = Color.White,
+                            fontFamily = displayFontFamily,
+                            fontSize = 14.sp,
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // TODO: MAKE THE REAL NAVIGATION
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+}
